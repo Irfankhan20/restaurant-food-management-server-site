74018b0f492b31153945b4c77aad24001e69f06c
Irfankhan20 <sipathan96@gmail.com>
2023-11-07 10:25:18 +0600

    food items post operation seccessful

--- index.js
+++ index.js
@@ -15,37 +15,68 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
 
-    // mongodb collection 
-    const foodsCollection = client.db("meetBox").collection("addFood");
+        // mongodb collection 
+        const foodsCollection = client.db("meetBox").collection("addFood");
 
-    //post operation of add a food item
-    app.post("/addfoods", async (req, res) => {
-        const newFood = req.body;
-        const result = await foodsCollection.insertOne(newFood);
-        res.send(result);
-      });
+        //post operation of add a food item
+        app.post("/addfoods", async (req, res) => {
+            const newFood = req.body;
+            const result = await foodsCollection.insertOne(newFood);
+            res.send(result);
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        });
+
+        // read foodItems data from mongo db
+        app.get("/allfoods", async (req, res) => {
+            const query = {};
+            const result = await foodsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        //find foodItems by email
+        app.get('/allfoods', async (req, res) => {
+            try {
+                let query = {};
+                if (req.query.email) {
+                    query = { addByEmail: req.query.email }; // Use "addByEmail" field to filter by email
+                }
+
+                const result = await foodsCollection.find(query).toArray();
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send("Internal Server Error");
+            }
+        });
+
+        //fooItems count operation
+        //get number of products
+        app.get('/foodItemsCount', async (req, res) => {
+            const count = await foodsCollection.estimatedDocumentCount();
+            res.send({ count })
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -53,8 +84,8 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("MeetBox Working");
-  });
+});
 
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`MeetBox Working on port ${port}`);
-  });
+});
